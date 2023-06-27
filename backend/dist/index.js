1b018f6fbf447ddfe0b3b7a636749c948bfa90e0
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-06-27 19:05:17 -0300

    mongoose

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,7 +2,7 @@
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _mongoose = require('mongoose'); var _mongoose2 = _interopRequireDefault(_mongoose);
-var _categoriesjson = require('../models/categories.json'); var _categoriesjson2 = _interopRequireDefault(_categoriesjson);
+var _categoriesjson = require('../models/categories.json');
 //Helpers
 var _FormProjectValidation = require('./helpers/FormProjectValidation'); var _FormProjectValidation2 = _interopRequireDefault(_FormProjectValidation);
 //Models
@@ -17,7 +17,7 @@ app.use(_express2.default.json())
 _mongoose2.default.connect('mongodb://0.0.0.0:27017/costs').then(() => console.log('Conectado ao MongoDB')).catch(err => console.log(err))
 //Rotas
 app.get('/categories', (req, res) => {
-	res.json(_categoriesjson2.default.categories)
+	res.json(_categoriesjson.categories)
 })
 app.post('/createproject', (req, res) => {
 	let verification = _FormProjectValidation2.default.call(void 0, req.body.name, req.body.budget, req.body.category)
@@ -33,12 +33,34 @@ app.post('/createproject', (req, res) => {
 	}
 })
 app.get('/projects', (req, res) => {
-	Project.find().lean().then(prj => {
-		res.send(prj)
+	Project.find(req.query.id? {_id: req.query.id} : {}).lean().then(prj => {
+		res.json(prj)
 	}).catch(err => {
 		console.log(err)
 		res.status(500).send()
 	})
+})
+app.post('/editprj', (req, res) => {
+	Project.findByIdAndUpdate(req.body._id, req.body, {new: true}).then(data => res.json(data)).catch(err => {
+		console.error(err)
+		res.status(500).send()
+	})
+})
+app.post('/delete', (req, res) => {
+	Project.findByIdAndDelete(req.body.id).then(() => res.status(200).send()).catch(err => {
+		console.log(err)
+		res.status(500).send()
+	})
+})
+app.post('/services', (req, res) => {
+	console.log(req.body)
+	Project.findByIdAndUpdate(req.body._id, req.body, {new: true}).then(data => {
+		console.log(data)
+		res.json(data)
+	}).catch(err => {
+        console.error(err)
+        res.status(500).send()
+    })
 })
 const port = process.env.PORT
 app.listen(port, () => console.log('Servidor ativo na porta ' + port))
